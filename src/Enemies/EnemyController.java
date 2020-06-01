22e9bd9af52117f4c9c1875b307a6f382f24c903
JQ H <TknHJQ@outlook.com>
2020-06-01 12:09:59 +0800

    在EnemyController中增加了敌人路径点和行动阶段

--- src/Enemies/EnemyController.java
+++ src/Enemies/EnemyController.java
@@ -8,6 +8,9 @@ import java.awt.*;
 
 public class EnemyController
 {
+	private Point[] wayPoint;//敌人单位的移动路径点
+	private int phase;//敌人走过的第几个路径阶段，第0个点和第1个点之间为第0阶段
+	
 	public static void moveToPoint(Point targetPoint)
 	{
 		//传入敌人单位，采用某种方式判断该单位所属的位置，参考敌人对象的velocity属性，使用setPosition改变其位置，并使用某种方式判断敌人是否应该转弯，若转弯则调用该对象的setTowards属性
